d67cb1725f838c84f596c8834efd17c7183f42dc
Akshay <akshayzapz7@gmail.com>
2023-10-02 12:01:16 +0530

    Initial commit

---
+++ lib/constants/image_constants.dart
@@ -0,0 +1,6 @@
+
+import 'package:flutter/material.dart';
+
+const bgImage = AssetImage("assets/images/bg img.png");
+
+const cover = AssetImage("assets/images/VIBGYO-1 1.png");
---
+++ lib/screens/splash_screen/splash_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import '../../constants/image_constants.dart';
+
+class SplashScreen extends StatelessWidget {
+  SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(image: bgImage),
+                ),
+              ),
+              Positioned(
+                bottom: 120,
+
+                child: Container(
+                  height: MediaQuery.of(context).size.height*0.3,
+                  width: MediaQuery.of(context).size.width*0.6,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(image: cover),
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+        ],
+      ),
+    );
+  }
+}
